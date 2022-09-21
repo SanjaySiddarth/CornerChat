878867d9b3399c9516dbbac67c2c21a168f3f9eb
SanjaySiddarth <sanjaysiddarth.vrao@gmail.com>
2022-09-21 22:57:29 +0530

    In between adding partial for messages

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,2 +1,8 @@
 class ChatroomController < ApplicationController
+
+    def chatroom
+        user = User.second
+        @messages = user.messages
+    end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
 class SessionsController < ApplicationController
 
     def login
+        user = User.second
+        @messages = user.messages
     end
     
 end
