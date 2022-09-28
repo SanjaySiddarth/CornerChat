11ee0af46a155fc8bd152e1a55be0d240bcc0cd1
SanjaySiddarth <sanjaysiddarth.vrao@gmail.com>
2022-09-28 18:27:09 +0530

    Add validations to message

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,14 +1,16 @@
 class MessageController < ApplicationController
 
     def create_message
-        logger.warn "===========Message received=============="
+        # Validations - Message content must not be null/empty
         # message_content = params[:message_input]
-        current_user = session[:user]
-        message = User.find_by(username: current_user).messages.build(body: params[:message_input])
-        if message.save
-        # Message.create!(user_id: User.find_by(username: session[:user]).id, body: message_content)
-            ActionCable.server.broadcast "chatroom_channel",
-                                        message_details: message_render(message)
+        if !params[:message_input].empty?
+            current_user = session[:user]
+            message = User.find_by(username: current_user).messages.build(body: params[:message_input])
+            if message.save
+            # Message.create!(user_id: User.find_by(username: session[:user]).id, body: message_content)
+                ActionCable.server.broadcast "chatroom_channel",
+                                            message_details: message_render(message)
+            end
         end
 
         # redirect_to chat_path
